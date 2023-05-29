a12a1892ef0bb91fcf52835807f9397fdf3a27b1
Mohamed Salah <mohamedsalahmasoud173@gmail.com>
2023-05-29 19:18:42 +0300

    adding dotenv

--- Backend/models/db.js
+++ Backend/models/db.js
@@ -1,8 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const mongoose = require('mongoose');
 
-const MONGO_URL = require('./dbConfigurations');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const user = process.env.USER_NAME;
+const password = process.env.MONGO_PASSWORD;
 
 const connection = async () => {
+  const MONGO_URL = `mongodb+srv://${user}:${password}@whatsappclone.kg3pnx7.mongodb.net/?retryWrites=true&w=majority`;
   try {
     await mongoose.connect(MONGO_URL);
     console.log('Connect to mongoDB');
